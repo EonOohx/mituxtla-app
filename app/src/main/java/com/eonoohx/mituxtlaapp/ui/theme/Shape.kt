8863b71286a9c3f31b6db0d7f8573b0497dd8351
EonOohx <RaulAlejandro_RodriguezR@hotmail.com>
2025-03-13 12:33:13 -0600

    Styles added

---
+++ app/src/main/java/com/eonoohx/mituxtlaapp/ui/theme/Shape.kt
@@ -0,0 +1,7 @@
+package com.eonoohx.mituxtlaapp.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shape = Shapes(small = RoundedCornerShape(8.dp))
--- app/src/main/java/com/eonoohx/mituxtlaapp/ui/theme/Type.kt
+++ app/src/main/java/com/eonoohx/mituxtlaapp/ui/theme/Type.kt
@@ -1,34 +1,30 @@
 package com.eonoohx.mituxtlaapp.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.eonoohx.mituxtlaapp.R
+
+val andikaBold = FontFamily(Font(resId = R.font.andika_bold))
+val daysOneRegular = FontFamily(Font(resId = R.font.days_one))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    headlineLarge = Typography().headlineLarge.copy(
+        fontFamily = daysOneRegular,
+        fontWeight = FontWeight.Bold
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    headlineMedium = Typography().headlineMedium.copy(
+        fontFamily = daysOneRegular,
+        fontWeight = FontWeight.Bold
+    ),
+    bodyLarge = Typography().bodyLarge.copy(
+        fontFamily = andikaBold,
+        fontSize = 20.sp
+    ),
+    labelLarge = Typography().labelLarge.copy(
+        fontFamily = andikaBold
     )
-    */
 )
